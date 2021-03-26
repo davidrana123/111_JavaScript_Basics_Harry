793104fe7985565b7840d81a7385b01fa8016921
David <david123rana@gmail.com>
2021-03-26 22:16:52 +0530

    Ways to print in javaScript,javaScript console,JavaScript variables, Data types in JavaScript,1- Primitive data types : undefined, null, number, string, boolean, symbol   2- Reference data types : Arrays and Objects

---
+++ index.js
@@ -0,0 +1,64 @@
+//1 - Ways to print in javaScript
+
+// console.log("Hello World");
+//alert("me");
+//document.wrie("this is document write")
+
+// 2. javaScript console ApplicationCache
+
+//  console.log("hello world");
+//  console.warn("this is warning");
+//  console.error("this is error");
+
+ // 3 - JavaScript variables
+ //What are variables ? ? - Containers to store data values.
+ var numb1 = 34;
+ var numb2 = 56;
+ console.log(numb1 + numb2);
+
+// 4- Data types in JavaScript
+//Number
+var num1 = 455;
+var num2 = 56.76;
+
+//String
+var str1 = "This is String";
+var str1 = "This is  String";
+
+//Objects (key, value)
+var marks = {
+  ravi: 45,
+  subham: 78,
+  david: 99.977
+}
+console.log(marks);
+
+//Boolems
+var a = true;
+var b = false;
+console.log(a);
+
+ // undefuned
+ var und;
+ console.log(und);
+
+ //null
+ var n = null;
+ console.log(n);
+
+//  At a very high level, there are two types of data types in JavaScript
+//  1- Primitive data types : undefined, null, number, string, boolean, symbol
+//  2- Reference data types : Arrays and Objects
+
+//Arrays  
+var arr = [1,2,"david",3,4,5]
+console.log(arr);
+console.log(arr[2]);
+
+
+
+
+
+
+
+
