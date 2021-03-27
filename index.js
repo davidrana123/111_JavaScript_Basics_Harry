7c0d28ceaf6cb9b20026742b49a333b39fb6d200
David <david123rana@gmail.com>
2021-03-27 22:10:25 +0530

    operators

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
  //What are variables ? ? - Containers to store data values.
  var numb1 = 34;
  var numb2 = 56;
- console.log(numb1 + numb2);
+//  console.log(numb1 + numb2);
 
 // 4- Data types in JavaScript
 //Number
@@ -31,20 +31,20 @@ var marks = {
   subham: 78,
   david: 99.977
 }
-console.log(marks);
+// console.log(marks);
 
 //Boolems
 var a = true;
 var b = false;
-console.log(a);
+// console.log(a);
 
  // undefuned
  var und;
- console.log(und);
+//  console.log(und);
 
  //null
  var n = null;
- console.log(n);
+//  console.log(n);
 
 //  At a very high level, there are two types of data types in JavaScript
 //  1- Primitive data types : undefined, null, number, string, boolean, symbol
@@ -52,8 +52,55 @@ console.log(a);
 
 //Arrays  
 var arr = [1,2,"david",3,4,5]
-console.log(arr);
-console.log(arr[2]);
+// console.log(arr);
+// console.log(arr[2]);
+
+//Operators in JavaScript
+
+// 1 - Arithmtic Operators
+// var a = 34;
+// var b = 56;
+// console.log("The value of a + b is", a+b);
+// console.log("The value of a - b is",a-b);
+// console.log("The value of a * b is",a*b);
+// console.log("The value of a / b is",a/b);
+
+// 2 - Assignment Operators
+var c = b;
+c +=2;
+// console.log(c);
+
+// 3 - comparison Operators
+var x = 34;
+var y = 56;
+// console.log(x == y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x > y);
+// console.log(x < y);
+
+//Logical and operators
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && false);
+// console.log(false && true);
+
+// logical or operators
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || false);
+// console.log(false || true);
+
+//such ko jhut our jhut jko such use logical not khte hai
+// console.log(!false);
+// console.log(!true);
+
+
+
+
+
+
 
 
 
